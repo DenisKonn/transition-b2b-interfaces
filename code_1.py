d86f1a9ae3a20b3c1d8b0b9fe8f1272e1bde0158
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-10-23 08:25:42 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,52 +1,14 @@
+if __name__ == "__main__":
+def generate_random_data():
+        print(f"Random Number: {item}")
+
+def main():
 import random
 
     for item in data:
-    return data
-    data = generate_random_data()
 
-def generate_random_data():
-
-
-if __name__ == "__main__":
-def main():
     main()
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")public class RandomDataGenerator {
-        }
-        for (int item : data) {
-}
-        }
-
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-    main()
-
-def generate_random_data():
-    for item in data:
-
-import random
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-if __name__ == "__main__":
-
     data = generate_random_data()
-def main():import random
-
-
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    for item in data:
-
-def main():
-    main()
     return data
-    data = generate_random_data()
-def generate_random_data():
 
     data = [random.randint(1, 100) for _ in range(10)]
